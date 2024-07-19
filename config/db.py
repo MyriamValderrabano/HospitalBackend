75458598253be021aab56e4f63ef5cecc46f5bb3
Myriam <210467@utxicotepec.edu.mx>
2024-07-19 14:20:12 -0600

    Cambios bd

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://avnadmin:AVNS_G5sGwfrbPtZVPUhHUS2@mysql-base-hospital-hospital-base.h.aivencloud.com:19854/defaultdb?ssl-mode=REQUIRED"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_G5sGwfrbPtZVPUhHUS2@mysql-base-hospital-hospital-base.h.aivencloud.com:19854/defaultdb"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
